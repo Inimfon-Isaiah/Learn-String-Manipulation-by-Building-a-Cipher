87861d6535ee13e56c0af5465860f965ac24c193
Inimfon Isaiah <inimfonisaiah19@gmail.com>
2025-04-25 09:08:39 +0100

    Refactor Caesar cipher implementation into a function

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 text = "Hello Zaira"
 shift = 3
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-encrypted_text = ""
 
-for char in text.lower():
-    if char == " ":
-        encrypted_text += char
-    else:
-        index = alphabet.find(char)
-        new_index = (index + shift) % 26
-        encrypted_text += alphabet[new_index]
-    print("char:", char, "encrypted text:", encrypted_text)
+
+# message and offset are parameters of the caesar function
+def caesar(message, offset):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    encrypted_message = ""
+
+    for char in message.lower():
+        if char == " ":
+            encrypted_message += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_message += alphabet[new_index]
+    print("plain message:", message)
+    print("encrypted message:", encrypted_message)
+
+
+caesar(text, shift)
